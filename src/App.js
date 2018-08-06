d4825836d9135f7895f75e8243b574fb5106a4c5
kokeshing <kou7cdb1a@gmail.com>
2018-08-07 06:21:06 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Components } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { Layout, Menu, Icon, Timeline, List, Avatar } from 'antd';
-import './reset.css'
+import Product from './Product';
+import './reset.css';
 import './App.css';
-const vaeimg: string = require('./img/vae.png');
-const gifimg: string = require('./img/movie.gif');
-const resultimg: string = require('./img/result.png');
 
-const { Header, Content, Sider, Footer } = Layout;
+const { Header, Content, Sider } = Layout;
 
 class App extends React.Component {
-    state = {
-        collapsed: window.parent.screen.width > 800 ? false : true,
-        collapsedwidth: window.parent.screen.width > 500 ? 80 : 0,
-    };
+    constructor() {
+        super();
+        this.state = {
+            collapsed: window.parent.screen.width > 800 ? false : true,
+            collapsedwidth: window.parent.screen.width > 500 ? 80 : 0,
+        };
+    }
 
     onCollapse = (collapsed) => {
         this.setState({ collapsed });
@@ -61,7 +62,7 @@ class App extends React.Component {
                     </Menu>
                 </Sider>
                 <Layout>
-                    <Header style={{ background: '#f2feff', padding: 0 }}>
+                    <Header style={{ background: 'white', padding: 0 }}>
                         <div id="header-wrapper">
                         <List>
                             <List.Item>
@@ -76,7 +77,7 @@ class App extends React.Component {
                         <div id="wrapper">
                             <Route exact path='/' component={home}/>
                             <Route path='/about.me' component={aboutme}/>
-                            <Route path='/product' component={product}/>
+                            <Route path='/product' component={Product}/>
                             <Route path='/blog' component={blog}/>
                             <Route path='/contact' component={contact}/>
                         </div>
@@ -90,8 +91,6 @@ class App extends React.Component {
 
 const home = () => (
     <div id="contents-wrapper">
-        <h1>KOTARO ONISHI</h1>
-        <h2>Japanese Kuso Engineer</h2>
         <h1 style={{ margin: 24 }}>Welcome!!</h1>
         <h1>🙇</h1>
     </div>
@@ -105,7 +104,7 @@ const aboutme = () => (
             <List>
                 <List.Item>
                     <List.Item.Meta avatar={<Avatar icon="calendar" />} title="Birth" description="1997"/>
-                    <List.Item.Meta avatar={<Avatar icon="team" />} title="Organization" description="UEC CAT2 "/>
+                    <List.Item.Meta avatar={<Avatar icon="team" />} title="Organization" description="UEC CAT2"/>
                     <List.Item.Meta avatar={<Avatar icon="home" />} title="Living" description="Tokyo"/>
                 </List.Item>
             </List>
@@ -125,26 +124,6 @@ const aboutme = () => (
     </div>
 )
 
-const product = () => (
-    <div id="contents-wrapper">
-        <h1>VAE</h1>
-        <p>Variational Autoencoder is one of the generate model.</p>
-        <div style={{ padding: 24, textAlign: 'left' }}>
-            <h2>Approach</h2>
-            <p>The input data denote x, and the latent variable denote z.<br/>The reason why we can in this way is this method basing on manifold hypothesis.<br/>And, suppose z is probably distribution N(0, 1).<br/>Encoder is work that processing x to z.<br/>Decoder is work that processing z to x.<br/>From the above, Encoder can be expressed q(z|x), Decoder can be expressed p(x|z).</p>
-            <div style={{ textAlign: 'center' }}><img src={vaeimg}/></div>
-            <h2>Implementation</h2>
-            <p>When implementation, not output z. the mean of z and dispersion of z.<br/>It is the solution of computational problems.</p>
-            <p>Loss function denote reconstraction loss and KL-divergence.<br/>It is because, reconstraction loss denote the diffrent of the input data x and the decoder output<br/>x^. KL-divergenece denote the diffrent of x and x^.</p>
-            <h2>Result</h2>
-            <h3>Training Trainsition</h3>
-            <div style={{ textAlign: 'center' }}><img src={gifimg}/></div>
-            <h3>Finally Result</h3>
-            <div style={{ textAlign: 'center' }}><img src={resultimg}/></div>
-        </div>
-    </div>
-)
-
 const blog = () => (
     <div id="contents-wrapper">
         <h1>🙇 now developing 🙇</h1>
@@ -153,7 +132,6 @@ const blog = () => (
 
 const contact = () => (
     <div id="contents-wrapper">
-        <h1>Someday, Inquiry form will be added</h1>
         <List>
             <List.Item>
                 <a href="https://twitter.com/kokeshing_" style={{ padding: 10 }}><List.Item.Meta avatar={<Avatar icon="twitter" />} title="Twitter" description=""/></a>
